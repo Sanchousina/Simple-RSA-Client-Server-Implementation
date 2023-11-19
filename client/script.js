603064b2df8cdc708072f6f7bfe2bb2c40572fcc
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2023-11-19 21:32:47 +0100

    First test route on frontend

---
+++ client/script.js
@@ -0,0 +1,2 @@
+axios.get('http://127.0.0.1:5000/')
+  .then(res => console.log(res.data));
--- server/main.py
+++ server/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
-  return "Home"
+  return jsonify({'data': 'Home'})
 
 if __name__ == "__main__":
   app.run(debug=True)
