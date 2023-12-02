798175884058a08e855529cd92d0630de27fe297
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2023-12-02 14:16:57 +0100

    Extract decrypting message into function

--- server/main.py
+++ server/main.py
@@ -56,25 +56,30 @@ def generateKeys():
 def register():
   data = request.get_json()
 
+  decrypted_name = decrypt_message(data['name'])
+  decrypted_password = decrypt_message(data['password'])
+  print('Decrypted name, password: ', decrypted_name, decrypted_password)
+
+  return jsonify(data), 201
+
+def decrypt_message(msg):
   private_key_dict = json.loads(private_key)
   n_decoded = int.from_bytes(base64.urlsafe_b64decode(private_key_dict['n'] + '=='), byteorder='big')
   d_decoded = int.from_bytes(base64.urlsafe_b64decode(private_key_dict['d'] + '=='), byteorder='big')
 
   decrypted_message_bytes = [
     long_to_bytes(pow(int(value), d_decoded, n_decoded))
-    for value in data['name']
+    for value in msg
   ]
   print(decrypted_message_bytes)
 
   try:
     decrypted_message = b''.join(decrypted_message_bytes)
     decrypted_text = decrypted_message.decode('utf-8')
-
-    print("Decrypted Message:", decrypted_text)
   except UnicodeDecodeError as e:
     print("Decoding as UTF-8 failed:", e)
 
-  return jsonify(data), 201
+  return decrypted_text
 
 if __name__ == "__main__":
   app.run(debug=True)
