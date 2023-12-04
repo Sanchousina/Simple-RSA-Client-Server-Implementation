346855debc9e9c88749d74ad56088f8c1ecca6c0
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2023-12-04 16:12:17 +0100

    Refactor decoding keys on server

--- server/main.py
+++ server/main.py
@@ -14,15 +14,23 @@ public_key = 0
 private_key = 0
 n_decoded = 0
 d_decoded = 0
+client_public_key = 0
+client_n_decoded = 0
+client_e_decoded = 0
 
 @app.route("/publicKey")
 def generateKeys():
   global public_key
   global private_key
+  global n_decoded
+  global d_decoded
 
   key = jwk.JWK.generate(kty='RSA', size=2048)
   public_key = key.export_public()
-  private_key = key.export_private()
+  private_key = json.loads(key.export_private())
+
+  n_decoded = decode_keys(private_key['n'])
+  d_decoded = decode_keys(private_key['d'])
 
   return jsonify({
     "publicKey": public_key
@@ -32,20 +40,20 @@ def generateKeys():
 def register():
   data = request.get_json()
 
+  client_public_key = data['clientPublicKey']
   decrypted_name = decrypt_message(data['name'])
   decrypted_password = decrypt_message(data['password'])
+  print('Client Public Key: ', client_public_key)
   print('Decrypted name, password: ', decrypted_name, decrypted_password)
 
   return jsonify({"name": decrypted_name}), 201
 
 def decrypt_message(msg):
-  decode_keys()
 
   decrypted_message_bytes = [
     long_to_bytes(pow(int(value), d_decoded, n_decoded))
     for value in msg
   ]
-  print(decrypted_message_bytes)
 
   try:
     decrypted_message = b''.join(decrypted_message_bytes)
@@ -55,14 +63,9 @@ def decrypt_message(msg):
 
   return decrypted_text
 
-def decode_keys():
-  global private_key
-  global n_decoded
-  global d_decoded
-
-  private_key_dict = json.loads(private_key)
-  n_decoded = int.from_bytes(base64.urlsafe_b64decode(private_key_dict['n'] + '=='), byteorder='big')
-  d_decoded = int.from_bytes(base64.urlsafe_b64decode(private_key_dict['d'] + '=='), byteorder='big')
+def decode_keys(key):
+  key_decoded = int.from_bytes(base64.urlsafe_b64decode(key + '=='), byteorder='big')
+  return key_decoded
 
 if __name__ == "__main__":
   app.run(debug=True)
